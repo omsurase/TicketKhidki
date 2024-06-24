c30a9aa3f7dacd5a9296139d24f3349988dd60c5
omsurase <om.surase21@vit.edu>
2024-06-25 03:11:30 +0530

    basic pages and stylesheets added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./stylesheets/alignments.css"
+import "./stylesheets/sizes.css"
+import "./stylesheets/form-elements.css"
+import "./stylesheets/theme.css"
+import "./stylesheets/customs.css"
 
 function App() {
   return (
     <div >
-      <h1>
-        Ticket Khidki
-      </h1>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "antd";
+
+function Register() {
+  return (
+    <div className='flex justify-center h-screen items-center'>
+        <Button type="primary">Primary Button</Button>
+        Register
+    </div>
+  )
+}
+
+export default Register
