80b04d3c9bae7823b109f06593d02d695a896991
omsurase <om.surase21@vit.edu>
2024-06-25 04:20:18 +0530

    Added Login and Register pages

---
+++ client/src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Button({ title, onclick, variant, disabled, fullWidth , type}) {
+    let className = "bg-primary p-1 text-white";
+
+    if (fullWidth) {
+    className += " w-full";
+    }
+    
+    if(variant === 'outlined'){
+    className = className.replace(
+      "bg-primary",
+      "border border-primary text-primary bg-white"
+    );
+  }
+
+
+  return (
+    <div>
+          <button className={className} type={ type }>
+              {title}
+          </button>
+    </div>
+  )
+}
+
+export default Button
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
-import { Button } from "antd";
-
+import { Form } from "antd";
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom'
 function Register() {
+    const onFinish = ( values ) => {
+        console.log(values)
+    };
   return (
-    <div className='flex justify-center h-screen items-center'>
-        <Button type="primary">Primary Button</Button>
-        Register
+    <div className='flex justify-center h-screen items-center bg-primary'>
+          <div className='card p-3 w-400'>
+              <h1 className="text-xl mb-1">
+                  Ticket Khidki
+              </h1>    
+                <hr />
+              <Form
+                  layout='vertical'
+                  className='mt-1'
+                  onFinish={ onFinish }>
+                  <Form.Item
+                      label= "Name"
+                      name = "name"
+                      rules={[{ required: true, message: "Please enter your name" }]}
+                    >
+                      <input type="text" />
+                  </Form.Item>
+                  <Form.Item
+                      label= "Email"
+                      name = "email"
+                      rules={[{ required: true, message: "Please enter your email" }]}
+                    >
+                      <input type="email" />
+                  </Form.Item>
+                  <Form.Item
+                      label= "Password"
+                      name = "password"
+                      rules={[{ required: true, message: "Please enter your password" }]}
+                    >
+                      <input type="password" />
+                  </Form.Item>
+                  <div className="flex flex-col mt-2 gap-1">
+                      <Button fullWidth type='submit' title='REGISTER' />
+                      <Link className='text-primary '  to="/login">
+                      Already have an account? <span style={{ textDecoration: 'underline' }}>Login</span>
+                      </Link>
+                  </div>
+
+              </Form>
+        </div>
     </div>
   )
 }
