e7afec238598576694fcbf4e22d3956c1b0e537e
omsurase <om.surase21@vit.edu>
2024-06-25 21:52:35 +0530

    Register frontend backend connected, post request error present.

--- client/src/apicalls/users.js
+++ client/src/apicalls/users.js
@@ -1,10 +1,22 @@
 const { axiosInstance } = require(".");
 
+//register user
 export const RegisterUser = async (payload) => { 
     try { 
         const response = await axiosInstance.post("/api/users/register", payload);
-        return response;
+        return response.data;
     } catch (err) { 
-        return err.response
+        return err.response;
+    }
+}
+
+//login user
+
+export const LoginUser = async (payload) => { 
+    try { 
+        const response = await axiosInstance.post("/api/users/login", payload);
+        return response.data;
+    } catch (err) { 
+        return err.response;
     }
 }
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Form } from "antd";
+import { Form, message } from "antd";
 import Button from '../../components/Button';
 import { Link } from 'react-router-dom'
+import { RegisterUser } from '../../apicalls/users';
+
 function Register() {
-    const onFinish = ( values ) => {
-        console.log(values)
+    const onFinish = async ( values ) => {
+      try {
+        const response = await RegisterUser(values);
+        if (response.success) {
+          message.success(response.message);
+        } else { 
+          message.error(response.message);
+        }
+      } catch (err) { 
+        message.error(err.message);
+      }
     };
   return (
     <div className='flex justify-center h-screen items-center bg-primary'>
