406780c6da65df8385182a372fd8264534e65abf
omsurase <om.surase21@vit.edu>
2024-06-26 04:19:30 +0530

    Redux setup.

---
+++ client/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import loaderReducer from "./loaderSlice"
+import usersReducer from "./userSlice"
+const store =  configureStore({
+    reducer: {
+        loaders: loaderReducer,
+        users: usersReducer,
+    },
+});
+
+export default store;
