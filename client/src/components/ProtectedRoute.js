c619f86c1114b6ee868587df356566ede46b01c0
omsurase <om.surase21@vit.edu>
2024-06-26 10:19:19 +0530

    Header and Homepage layout added.

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -39,13 +39,37 @@ function ProtectedRoute({ children }) {
             navigate("/login");
         }
     }, []);
-    return (
-        user && <div>
-            {user.name}
 
-            {children}
+
+    return (
+    user && (
+      <div className="layout p-1">
+        <div className="header bg-primary flex justify-between p-2">
+          <div>
+            <h1 className="text-2xl text-white cursor-pointer"
+              onClick={() => navigate("/")}
+            >TICKET KHIDKI</h1>
+          </div>
+
+          <div className="bg-white p-1 flex gap-1">
+            <i className="ri-shield-user-line text-primary"></i>
+            <h1 className="text-sm">
+              {user.name}
+            </h1>
+
+            <i
+              className="ri-logout-box-r-line ml-2"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
+            ></i>
+          </div>
         </div>
+        <div className="content mt-1 p-1">{children}</div>
+      </div>
     )
+  );
 }
 
 export default ProtectedRoute
