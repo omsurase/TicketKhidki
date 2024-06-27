c9a046b95ac1ec3fffdefb06b81078ef562447c3
omsurase <om.surase21@vit.edu>
2024-06-27 12:44:11 +0530

    Shows modal ui.

--- client/src/pages/Profile/shows/index.js
+++ client/src/pages/Profile/shows/index.js
@@ -1,8 +1,44 @@
-import { Modal } from 'antd'
-import React from 'react'
+import { Modal, Table } from 'antd'
+import React, { useState } from 'react'
+import Button from '../../../components/Button';
 
-function Shows({ openShowsModal, setOpenShowsModal, theater })
-{
+function Shows({ openShowsModal, setOpenShowsModal, theater }) {
+    const [view, setView] = useState("table");
+    const [shows, setShows] = useState([]);
+    const columns = [
+        {
+            title: "Show Name",
+            dataIndex: "name"
+        },
+        {
+            title: "Date",
+            dataIndex: "date"
+        },
+        {
+            title: "Time",
+            dataIndex: "time"
+        },
+        {
+            title: "Movie",
+            dataIndex: "movie"
+        },
+        {
+            title: "Ticket Price",
+            dataIndex: "ticketPrice"
+        },
+        {
+            title: "Total Seats",
+            dataIndex: "totaleSeats"
+        },
+        {
+            title: "Availaible Seats",
+            dataIndex: "available seats"
+        },
+        {
+            title: "Action",
+            dataIndex: "action"
+        },
+    ];
     return (
         <Modal
             title=""
@@ -11,9 +47,27 @@ function Shows({ openShowsModal, setOpenShowsModal, theater })
             width={1400}
             footer={null}
         >
-            <h1 className="text-primary-text-xl">
-                theater: {theater.name}
+            <h1 className="text-primary text-md uppercase mb-1">
+                Theater: {theater.name}
             </h1>
+            <hr />
+
+            <div className="flex justify-between mt-1 mb-1 items-center">
+
+                <h1 className='text-md uppercase'>
+                    {view === "table" ? "Shows" : "Add Show"}
+                </h1>
+
+                <Button
+                    variant="outlined"
+                    title="Add Show"
+                    onClick={() => {
+                        setView("form");
+                    }}
+                />
+            </div>
+
+            {view === "table" && <Table columns={columns} dataSource={shows} /> }
         </Modal>
     )
 }
