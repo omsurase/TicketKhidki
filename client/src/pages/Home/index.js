0d9d1f7f9de06487ca9a1103c7dcc796228015d9
omsurase <om.surase21@vit.edu>
2024-06-28 00:00:45 +0530

    Homepage added.

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import {  Col, Row, message } from 'antd';
+import { useDispatch } from 'react-redux';
+import { HideLoading, ShowLoading } from '../../redux/loaderSlice';
+import { GetAllMovie } from '../../apicalls/movies';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+
+  const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
+  const getData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await GetAllMovie();
+      if (response.success) {
+        setMovies(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(HideLoading());
+    } catch (err) {
+      dispatch(HideLoading());
+      message.error(err.message);
+    }
+  };
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
-      Home
+      <input type='text'
+        className='search-input'
+        placeholder='Search for movies'
+      />
+
+      <Row
+        gutter={[20]}
+        className='mt-1'
+      >
+        {movies.map((movie) => (
+          <Col span={6}>
+            <div className="card flex flex-col gap-1 cursor-pointer"
+              onClick={() => navigate(`/movie/${movie._id}`)}
+            >
+              <img src={movie.poster} alt="" height={200} />
+              <div className="flex justify-center gap-1">
+                <h1 className="text-md uppercase">
+                  {movie.title}
+                </h1>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
     </div>
   )
 }
